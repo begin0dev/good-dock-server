20e4a4d1e6eb246eea704f629e3eda00b12dae15
begin0dev <prosonic1@gmail.com>
2022-03-12 17:41:23 +0900

    ADD - github action

--- src/socials/socials.controller.ts
+++ src/socials/socials.controller.ts
@@ -5,19 +5,27 @@ import { FindUserDto } from './dtos/findUser.dto';
 import { UsersService } from '../users/users.service';
 import { CurrentUser, UserSerializer } from '../serializers/user.serializer';
 import { JsendReturnType } from '../types/jsend.types';
+import { TokensService } from '../tokens/tokens.service';
 import modelSerializer from '../helpers/model-serializer';
 
 @Controller('socials')
 @UseGuards(AuthGuard(authTarget.VISITOR))
 export class SocialsController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private usersService: UsersService, private tokenService: TokensService) {}
 
   @Post()
-  async login(@Body() findUserDto: FindUserDto): Promise<JsendReturnType<CurrentUser>> {
+  async login(
+    @Body() findUserDto: FindUserDto,
+  ): Promise<JsendReturnType<CurrentUser, { accessToken: string; refreshToken: string }>> {
     const user = await this.usersService.findOrCreate(findUserDto.provider, findUserDto.accessToken);
-    console.log(user);
+    const userJson = modelSerializer(user, UserSerializer);
+    const accessToken = this.tokenService.generateAccessToken({ user: userJson });
     return {
-      payload: modelSerializer(user, UserSerializer),
+      payload: userJson,
+      meta: {
+        accessToken,
+        refreshToken: user.refreshToken,
+      },
     };
   }
 }
--- src/socials/socials.module.ts
+++ src/socials/socials.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { SocialsController } from './socials.controller';
 import { UsersModule } from '../users/users.module';
+import { TokensModule } from '../tokens/tokens.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TokensModule],
   controllers: [SocialsController],
 })
 export class SocialsModule {}
